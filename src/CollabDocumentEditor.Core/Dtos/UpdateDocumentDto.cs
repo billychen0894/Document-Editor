fed7f86b8fe4d651d4e18c3272a4e79a309301c3
billychen0894 <billychen0894@gmail.com>
2024-10-22 22:45:34 -0700

    update: UpdateDocumentDto Id property type to be Guid

--- src/CollabDocumentEditor.Core/Dtos/UpdateDocumentDto.cs
+++ src/CollabDocumentEditor.Core/Dtos/UpdateDocumentDto.cs
@@ -2,7 +2,7 @@ namespace CollabDocumentEditor.Core.Dtos;
 
 public class UpdateDocumentDto
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
     // UpdatedAt will be set by service
