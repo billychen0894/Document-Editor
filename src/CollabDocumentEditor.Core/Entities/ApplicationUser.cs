2952bfe59638472fb945b1c1c9ff96687eddadda
billychen0894 <billychen0894@gmail.com>
2024-10-25 22:45:07 -0700

    update: add `RefreshToken` and `RefreshTokenExprieTime` to ApplicationUser entity

--- src/CollabDocumentEditor.Core/Entities/ApplicationUser.cs
+++ src/CollabDocumentEditor.Core/Entities/ApplicationUser.cs
@@ -7,6 +7,8 @@ public class ApplicationUser : IdentityUser
     // Custom properties
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryTime { get; set; }
     public DateTime CreatedAt { get; set; }
     
     public virtual ICollection<Document> Documents { get; set; } // Navigation property
