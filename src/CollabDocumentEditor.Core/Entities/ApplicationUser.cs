cc3e8ee072dfad2d0befead318f7dcb3301f88dc
billychen0894 <billychen0894@gmail.com>
2024-10-26 22:16:43 -0700

    update: remove RefreshTokenExpiryTime nullish type

--- src/CollabDocumentEditor.Core/Entities/ApplicationUser.cs
+++ src/CollabDocumentEditor.Core/Entities/ApplicationUser.cs
@@ -8,7 +8,7 @@ public class ApplicationUser : IdentityUser
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string? RefreshToken { get; set; }
-    public DateTime? RefreshTokenExpiryTime { get; set; }
+    public DateTime RefreshTokenExpiryTime { get; set; }
     public DateTime CreatedAt { get; set; }
     
     public virtual ICollection<Document> Documents { get; set; } // Navigation property
