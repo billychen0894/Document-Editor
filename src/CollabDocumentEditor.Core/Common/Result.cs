acff694003b1d8bc677acc0b1ac8a3a86ede6b11
billychen0894 <billychen0894@gmail.com>
2024-11-06 21:03:02 -0800

    add: common Result model for http response

---
+++ src/CollabDocumentEditor.Core/Common/Result.cs
@@ -0,0 +1,18 @@
+namespace CollabDocumentEditor.Core.Common;
+
+public class Result<T>
+{
+    public bool IsSuccess { get; }
+    public T Value { get; }
+    public string Error { get; }
+
+    public Result(bool isSuccess, T value, string error)
+    {
+        IsSuccess = isSuccess;
+        Value = value;
+        Error = error;
+    }
+    
+    public static Result<T> Success(T value) => new Result<T>(true, value, string.Empty);
+    public static Result<T> Failure(string error) => new Result<T>(false, default, error);
+}
